2496fc15eeeb7a6316a4ab9d98de4c94f550e238
shanegibney <artgibney@gmail.com>
2016-07-08 23:38:26 +0100

    created DATABASES = {'default': dj_database_url.config()} in settings.py

--- mathsProject/settings.py
+++ mathsProject/settings.py
@@ -14,6 +14,7 @@ import os
 # Update database configuration with $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config()
+DATABASES = {'default': dj_database_url.config()}
 DATABASES['default'].update(db_from_env)
 
 
