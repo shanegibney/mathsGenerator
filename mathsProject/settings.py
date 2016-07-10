2da2f6761847bb8cc56c263303065b2a41dff409
shanegibney <artgibney@gmail.com>
2016-07-11 00:31:18 +0100

    static files paths in settings.py

--- mathsProject/settings.py
+++ mathsProject/settings.py
@@ -135,23 +135,37 @@ USE_L10N = True
 USE_TZ = True
 
 
+# # Static files (CSS, JavaScript, Images)
+# # https://docs.djangoproject.com/en/1.9/howto/static-files/
+#
+# STATIC_URL = '/static'
+#
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "/static"),
+# )
+#
+# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+#
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+#
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
-
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
+
 
 
 # Simplified static file serving.
